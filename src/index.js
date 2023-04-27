225fbe99061c4e0e64db7c91a3a9f9b2e94ec41f
DmytroBrekalo <brekalo4@gmail.com>
2023-04-27 13:09:30 +0300

    G

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from '../fetchCountries';
 import listOfCountries from './templates/listOfCountries.hbs';
 import oneCountryCard from './templates/oneCountryCard.hbs';
 
